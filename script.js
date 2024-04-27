31954ddcdc466195dc5a0ec440e7e2121126b853
alisonnbarbosa <alisonnbarbosaa@gmail.com>
2024-04-27 18:35:52 -0300

    v3

--- script.js
+++ script.js
@@ -42,9 +42,9 @@ function buttonPosition(position){
     if(position == 'center'){
         trocarTema('#e4e4e7','#fafafa','#a1a1aa','#e4e4e7','#0891b2','#ea580c','#78716c','#164e63','#7c2d12','#0c0a09','#f8fafc')
     } else if(position == 'end'){
-        trocarTema()
+        trocarTema('#2a0646','#3b0764','#3b0764','#9333ea','#581c87','#22d3ee','#d946ef','#c026d3','#67e8f9','#fde047','')
     } else{
-        trocarTema()
+        location.reload()//remover isso daqui e passar os parâmetros
     }
 }
 
@@ -80,30 +80,3 @@ function bgText(bg1,dp,bg2,text){
     btTheme.style.backgroundColor = bg2
     header.style.color = text
 }
-/**
-tema2:
-
-item.style.backgroundColor = bt2
-item.style.boxShadow = `inset 0px -5px 0px ${sd2}`
-
-bg- #e4e4e7
-bgContainer - #a1a1aa
-bgbuttons1 - #e4e4e7
-bgbuttons2 - #0891b2
-bgbuttons3 - #ea580c
-sd1- #78716c
-sd2 - #164e63
-sd3 - #7c2d12
-display - #e4e4e7
-text - #0c0a09
-* 
- * document.body.style.backgroundColor = bg
-    buttonsContainer.style.backgroundColor = bg2
-    header.style.color = color1
-
-    buttons.forEach((item)=>{
-        if(item.id == 'del'){
-            item.style.backgroundColor = bg2
-        }
-    })
- */
